1ac96be74ed874663a5a773e368ecc547af7d00c
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-16 13:47:35 +0800

    feat: create helper scripts, complete game backbone

--- src/script.js
+++ src/script.js
@@ -1,5 +1,8 @@
 // Imports
-
+import { updateGround, setupGround } from "./helpers/ground.js";
+import { updatePlayer, setupPlayer, getPlayerRect, setPlayerLose  } from "./helpers/player.js";
+import { updateObstacle, setupObstacle, getObstacleRects } from "./helpers/obstacle.js";
+import { updateNft, setupNft, getNftRects } from "./helpers/nft.js";
 
 // Define functions 
 function setPixelToGameScale() {
@@ -27,12 +30,76 @@ function update(time) {
     updateObstacle(delta, speedScale);
     updateNft(delta, speedScale);
     updateScore(delta);
-    updateObstacle(delta);
+    updateSpeedScale(delta);
     checkIfWeGotNft();
 
     if (checkLose()) return handleLose();
     lastTime = time;
     window.requestAnimationFrame(update);
+}
+
+function checkLose() {
+    const playerRect = getPlayerRect();
+    return getObstacleRects().some(rect => isCollision(rect, playerRect))
+}
+
+function isCollision(rect1, rect2) {
+    return (
+        rect1.left < rect2.right &&
+        rect1.top < rect2.bottom &&
+        rect1.right > rect2.left &&
+        rect1.bottom > rect2.top
+    )
+}
+
+function checkIfWeGotNft() {
+    const playerRect = getPlayerRect();
+    if (getNftRects().some(rect => isCollision(rect, playerRect))) {
+        const nftToRemove = document.querySelectorAll("[data-nft]")[0];
+        nftToRemove.remove();
+        nftScore += 1;
+        nftScoreElem.textContent = `NFT score: ${nftScore}`;
+    }
+    return getNftRects().some(rect => isCollision(rect, playerRect))
+}
+
+function updateSpeedScale(delta) {
+    speedScale += delta * SPEED_SCALE_INCREASE;
+}
+
+function updateScore(delta) {
+    score += delta * 0.01;
+    scoreElem.textContent = `Wei score: ${Math.floor(score)}`
+}
+
+function handleStart() {
+    lastTime = null;
+    speedScale = 1;
+    score = 0;
+    setupGround();
+    setupPlayer();
+    setupObstacle();
+    setupNft();
+    startScreenElem.classList.add("hide");
+    // call this only when screen refershes
+    window.requestAnimationFrame(update);
+}
+
+function handleLose() {
+    window.totalNFTScore += nftScore;
+    window.totalGweiScore += Math.floor(score);
+    nftScore = 0;
+
+    nftTotalScoreElem.textContent = `NFT total score: ${window.totalNFTScore}`;
+    gweiTotalScoreElem.textContent = `Wei total score: ${window.totaGweiScore}`;
+    nftScoreElem.textContent = `NFT Score: ${nftScore}`;
+
+    setPlayerLose()
+
+    setTimeout(() => {
+        document.addEventListener("keydown", handleStart, {once: true});
+        startScreenElem.classList.remove("hide");
+    }, 100)
 }
 
 // Game settings
@@ -46,7 +113,7 @@ const scoreElem = document.querySelector("[data-score]");
 const startScreenElem = document.querySelector("[data-start-screen]");
 const gweiTotalScoreElem = document.querySelector("[data-wei-total-score]");
 const nftTotalScoreElem = document.querySelector("[data-nft-total-score]");
-const nfScoreElem = document.querySelector("[data-nft-score]");
+const nftScoreElem = document.querySelector("[data-nft-score]");
 
 // Resize game canvas on screen resize
 setPixelToGameScale();
@@ -60,6 +127,9 @@ let lastTime;
 let speedScale;
 let score;
 let nftScore = 0;
+
+window.totalNFTScore = 0;
+window.totalGweiScore = 0;
 
 // Animate frames
 window.requestAnimationFrame(update);
